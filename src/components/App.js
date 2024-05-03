4b63f84c8cee08bdd9f7fd960149a37f2b68cc29
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-05-03 13:01:06 +0300

    completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+export default function App (){
+
+    const [randomImage, setRandomImage] = useState([]); // we can also set this to null
+    const [isLoading, setIsLoading] = useState(true); 
+
+    useEffect(() => {
+        fetch ("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => {
+                setRandomImage(data.message);
+                setIsLoading(false);
+            })
+            .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+    return (
+        <>
+           {isLoading ? (<p>Loading...</p>) : (<img src={randomImage} alt="A Random Dog" />)}
+        </>
+    );
+}
